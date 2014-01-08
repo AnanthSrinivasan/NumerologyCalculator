8efbf0271329887263844a36948b2833f7f72d98
amanoharan <amanoharan@paypal.com>
2014-01-09 00:36:52 +0530

    Fix Removed unnecessary js files

--- public/dist/js/custom.js
+++
@@ -1,8 +0,0 @@
-$('#btngenlink').tooltip({
-    'show': true,
-    'placement': 'bottom',
-    'title': "Generates links for Attendance..."
-});
-
-$.validator.messages.required = 'required';
-$('form[data-novalidate=yes]').bootstrapValidate();
